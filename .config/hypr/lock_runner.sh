ee87928d890fbbc164c4edad1b93b79ecf29f17f
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-06-01 00:13:10 -0300

    Remoção de cores dinâmicas na fonte do hyprlock pois ficava feio em alguns casos

--- .config/hypr/lock_runner.sh
+++ .config/hypr/lock_runner.sh
@@ -8,19 +8,9 @@ CACHE_FILE="$HOME/.cache/hyprlock_colors.conf"
 IMG=$(find "$DIR" -type f -iregex '.*\.\(png\|jpg\|jpeg\)' | shuf -n 1)
 [ -z "$IMG" ] && exit 1
 
-# calcula brilho
-BRIGHTNESS=$(magick "$IMG" -colorspace Gray -format "%[fx:int(mean*255)]" info:)
-
-# verifica se o brilho geral da imagem é maior ou menor que 128 e decide o contraste
-if [ "$BRIGHTNESS" -gt 128 ]; then
-    # fundo claro semi-transparente e texto escuro
-    ACCENT="ff333333"      # Opaco (ff) + cinza escuro
-    INPUT_BG="ccffffff"    # 80% alpha (cc) + branco
-else
-    # fundo escuro semi-transparente e texto branco
-    ACCENT="ffffffff"      # Opaco (ff) + branco
-    INPUT_BG="cc333333"    # 80% alpha (cc) + cinza escuro
-fi
+# fundo escuro semi-transparente e texto branco
+ACCENT="ffffffff"      # Opaco (ff) + branco
+INPUT_BG="cc333333"    # 80% alpha (cc) + cinza escuro
 
 # salva as cores em variáveis num arquivo de cache
 # tem o 0x na frente para indicar hexadecimal com opacidade
